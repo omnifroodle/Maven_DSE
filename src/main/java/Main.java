68ba5d05fb3648fa3efc679cb2d3a9395e5f132d
Matt Overstreet <matt.overstreet@gmail.com>
2017-04-20 11:15:07 -0400

    cleanup for readability, added async example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@ import com.datastax.driver.core.*;
 import com.datastax.driver.dse.graph.GraphResultSet;
 import com.datastax.driver.dse.graph.GraphStatement;
 import com.datastax.driver.dse.graph.SimpleGraphStatement;
+// for async access
+import com.google.common.base.Function;
+import com.google.common.util.concurrent.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -27,6 +31,9 @@ public class Main {
             String last_name = "Sixt";
             executeBoundInsert(session, first_name, last_name);
 
+            executeAsyncQuery(cluster);
+
+
             // Additional DSE features
             // Graph
             executeGraphQuery(session);
@@ -39,6 +46,70 @@ public class Main {
         } finally {
             if (cluster != null) cluster.close();                                        // (5)
         }
+    }
+
+    private static void getVersion(DseSession session) {
+        Row row = session.execute("select release_version from system.local").one(); // (3)
+        System.out.format("Cassandra Release Version: %s\n", row.getString("release_version")); // (4)
+    }
+
+    private static void executeSelect(DseSession session) {
+        System.out.println("Finding a row with a simple statement");
+
+        ResultSet rs = session.execute("select * from search.people where last_name = 'Smith' and first_name = 'John';");
+
+        for (Row person : rs) {
+            System.out.format("%s, %s\n", person.getString("last_name"), person.getString("first_name"));
+        }
+    }
+
+    private static void executeBoundInsert(DseSession session, String first_name, String last_name) {
+        System.out.println("Insert a row with a prepared statement");
+
+        PreparedStatement prepared = session.prepare("Insert into search.people (last_name, first_name) values (?, ?)");
+
+        BoundStatement bound = prepared.bind(last_name, first_name);
+        session.execute(bound);
+
+        // Now reverse the name, to demo prepared statements
+        bound = prepared.bind(first_name, last_name);
+        session.execute(bound);
+    }
+
+    // From http://docs.datastax.com/en/developer/java-driver-dse/1.2/manual/async/
+    private static void executeAsyncQuery(DseCluster cluster) {
+        // For historical reasons, this returns a Future of Session, not DseSession. But you can safely cast to DseSession in
+        // your callbacks if you need to access DSE-specific features. Here it doesn't really matter because we only use CQL
+        // features.
+        ListenableFuture<Session> session = cluster.connectAsync();
+
+        // Use transform with an AsyncFunction to chain an async operation after another:
+        ListenableFuture<ResultSet> resultSet = Futures.transform(session,
+                new AsyncFunction<Session, ResultSet>() {
+                    public ListenableFuture<ResultSet> apply(Session session) throws Exception {
+                        return session.executeAsync("select release_version from system.local");
+                    }
+                });
+
+        // Use transform with a simple Function to apply a synchronous computation on the result:
+        ListenableFuture<String> version = Futures.transform(resultSet,
+                new Function<ResultSet, String>() {
+                    public String apply(ResultSet rs) {
+                        return rs.one().getString("release_version");
+                    }
+                });
+
+        // Use a callback to perform an action once the future is complete:
+        Futures.addCallback(version, new FutureCallback<String>() {
+            public void onSuccess(String version) {
+                System.out.printf("DSE version: %s%n", version);
+            }
+
+            public void onFailure(Throwable t) {
+                System.out.printf("Failed to retrieve the version: %s%n",
+                        t.getMessage());
+            }
+        });
     }
 
     private static void executeGraphQuery(DseSession session) {
@@ -66,31 +137,6 @@ public class Main {
 
         System.out.format("%d records for last_name that sound like Jones", rs.one().getLong(0));
     }
-    private static void executeBoundInsert(DseSession session, String first_name, String last_name) {
-        System.out.println("Insert a row with a prepared statement");
 
-        PreparedStatement prepared = session.prepare("Insert into search.people (last_name, first_name) values (?, ?)");
 
-        BoundStatement bound = prepared.bind(last_name, first_name);
-        session.execute(bound);
-
-        // Now reverse the name, to demo prepared statements
-        bound = prepared.bind(first_name, last_name);
-        session.execute(bound);
-    }
-
-    private static void executeSelect(DseSession session) {
-        System.out.println("Finding a row with a simple statement");
-
-        ResultSet rs = session.execute("select * from search.people where last_name = 'Smith' and first_name = 'John';");
-
-        for (Row person : rs) {
-            System.out.format("%s, %s\n", person.getString("last_name"), person.getString("first_name"));
-        }
-    }
-
-    private static void getVersion(DseSession session) {
-        Row row = session.execute("select release_version from system.local").one(); // (3)
-        System.out.format("Cassandra Release Version: %s\n", row.getString("release_version")); // (4)
-    }
 }
